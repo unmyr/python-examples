fd0e65785e83fcaa90038c1ed2e853ef3f7bb212
unmyr <56982541+unmyr@users.noreply.github.com>
2021-03-27 00:57:20 +0900

    feat: Add random plot examples

---
+++ src/matplotlib/matplotlib_plot_random_line_graph.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.randn(30)
+y = np.sin(x) + np.random.randn(30)
+# plt.plot(x, y, "o")  # "o"は小さい円(circle marker)
+plt.plot(x, y)  # "o"は小さい円(circle marker)
+# plt.show()
+plt.savefig('matplotlib_plot_randam_line_graph.png')
---
+++ src/matplotlib/matplotlib_plot_random_point_diagram.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.randn(30)
+y = np.sin(x) + np.random.randn(30)
+plt.plot(x, y, "o")  # "o"は小さい円(circle marker)
+plt.savefig('matplotlib_plot_random_point_diagram.png')
