c150878f34ab1a7abf3bb04601b8eb0f10d89c16
unmyr <56982541+unmyr@users.noreply.github.com>
2021-03-28 17:10:02 +0900

    feat: Add code example of factorial function.

---
+++ src/factorial_iterative.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Calculate the factorial of a number."""
+from sys import argv
+
+
+def factorial(number: int) -> int:
+    """Calculate factorial."""
+    if number < 0:
+        return None
+
+    factor = 1
+    for n_cur in range(1, number + 1):
+        factor *= n_cur
+    return factor
+
+
+if __name__ == '__main__':
+    print(factorial(int(argv[1])))
+
+# EOF
