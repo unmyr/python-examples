c47d7751ba3574a6c47d81e184ee5876528d22f6
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-03 15:11:59 +0900

    ci: Remove a unnecessary module.

--- src/string/formatting_floats.py
+++
@@ -1,7 +0,0 @@
-"""Formatting floats."""
-import math
-if __name__ == "__main__":
-    print("PI*100 = %g" % (math.pi * 100))
-    print("PI*100 = %.3g" % (math.pi * 100))
-    print("PI*100 = %.6g" % (math.pi * 100))
-    print("PI*100 = %.20g" % (math.pi * 100))
