e1867cb67ee2c12999baac6950923bc9947a89e3
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-07 22:21:25 +0900

    feat: Add a example of pg8000.

---
+++ database/pg8000/src/pg8000_select.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""Example of pg8000."""
+import os
+import traceback
+
+import pg8000
+
+
+def main():
+    """Run main."""
+    pg_host = os.environ.get('PGHOST')
+    pg_database = os.environ.get('PGDATABASE')
+    pg_user = os.environ.get('PGUSER')
+    pg_password = os.environ.get('PGPASSWORD')
+
+    conn = None
+    cur = None
+    try:
+        conn = pg8000.connect(
+            host=pg_host,
+            database=pg_database,
+            user=pg_user,
+            password=pg_password
+        )
+        cur = conn.cursor()
+        cur.execute("""SELECT * from fruits_menu""")
+        rows = cur.fetchall()
+        print("\nShow records:\n")
+        for row in rows:
+            print("   ", row[0])
+    except pg8000.dbapi.ProgrammingError as exc:
+        print(traceback.format_exc())
+        print(exc)
+    except pg8000.dbapi.DatabaseError as exc:
+        print(traceback.format_exc())
+        print(exc)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
