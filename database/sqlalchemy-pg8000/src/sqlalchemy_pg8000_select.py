1f1e7fcd753cbdf8021c1816c33482e9aa4a6189
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-07 22:55:47 +0900

    feat: Add a example of sqlalchemy+pg8000

---
+++ database/sqlalchemy-pg8000/src/sqlalchemy_pg8000_select.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Example of postgresql+pg8000."""
+import os
+import traceback
+
+import sqlalchemy
+from sqlalchemy import text
+
+
+def main(engine):
+    """Run main."""
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(
+                text("SELECT * from fruits_menu")
+            )
+            for row in result:
+                print(f"name={row['name']} price={row['price']}", )
+
+    except sqlalchemy.exc.ProgrammingError as exc:
+        print(traceback.format_exc())
+        print(exc)
+
+
+if __name__ == '__main__':
+    engine = sqlalchemy.create_engine(
+        sqlalchemy.engine.URL.create(
+            'postgresql+pg8000',
+            host=os.environ.get('PGHOST'),
+            port=os.environ.get('PGPORT'),
+            database=os.environ.get('PGDATABASE'),
+            username=os.environ.get('PGUSER'),
+            password=os.environ.get('PGPASSWORD')
+        )
+    )
+    main(engine)
+
+# EOF
