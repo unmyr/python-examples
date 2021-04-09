ba008af0bd3776f676b24805b3b803fcd04b8d71
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-10 01:49:00 +0900

    refactor: Replace "print"s to "logger".

--- database/sqlalchemy-pg8000/src/sqlalchemy_pg8000_select.py
+++ database/sqlalchemy-pg8000/src/sqlalchemy_pg8000_select.py
@@ -1,25 +1,56 @@
 # -*- coding: utf-8 -*-
 """Example of postgresql+pg8000."""
+import json
 import os
 import traceback
+from logging import getLogger, StreamHandler, DEBUG, Formatter
 
 import sqlalchemy
 from sqlalchemy import text
 
 
+logger = getLogger(__name__)
+stream_handler = StreamHandler()
+stream_handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(stream_handler)
+logger.propagate = False
+stream_handler.setFormatter(
+    Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+)
+
+
 def main(engine):
     """Run main."""
+    result = {
+        'statusCode': 500,
+        'body': 'Internal Server Error.'
+    }
     try:
+        query_results = []
+        logger.info('engine.connect()')
         with engine.connect() as connection:
-            result = connection.execute(
-                text("SELECT * from fruits_menu")
+            logger.info('connection.execute()')
+            rows = connection.execute(
+                text("SELECT * FROM FruitsMenu")
             )
-            for row in result:
-                print(f"name={row['name']} price={row['price']}", )
+            for row in rows:
+                query_results.append({'name': row['name'], 'price': row['price']})
+                # print(f"name={row['name']} price={row['price']}")
+        return {
+            'statusCode': 200,
+            'body': json.dumps(query_results)
+        }
 
     except sqlalchemy.exc.ProgrammingError as exc:
         print(traceback.format_exc())
         print(exc)
+        result = {
+            'statusCode': 500,
+            'body': json.dumps(traceback.format_exc())
+        }
+
+    return result
 
 
 if __name__ == '__main__':
@@ -33,6 +64,6 @@ if __name__ == '__main__':
             password=os.environ.get('PGPASSWORD')
         )
     )
-    main(engine_pg8000)
+    print(main(engine_pg8000))
 
 # EOF
