0833adb6d2eb2af303c20656d2707c68f2697851
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-11 21:34:17 +0900

    feat: Commonize a code.

---
+++ database/sqlalchemy-postgresql/src/sqlalchemy_execute_select.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""Example of execute SELECT."""
+from contextlib import contextmanager
+from logging import getLogger, StreamHandler, DEBUG, Formatter
+import json
+import os
+import sys
+import time
+import traceback
+
+import sqlalchemy
+from sqlalchemy import text
+
+
+logger = getLogger(__name__)
+stream_handler = StreamHandler()
+stream_handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(stream_handler)
+logger.propagate = False
+stream_handler.setFormatter(
+    Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+)
+
+
+@contextmanager
+def create_engine(driver_name: str):
+    """Create engine."""
+    engine = sqlalchemy.create_engine(
+        sqlalchemy.engine.URL.create(
+            driver_name,
+            host=os.environ.get('PGHOST'),
+            port=os.environ.get('PGPORT'),
+            database=os.environ.get('PGDATABASE'),
+            username=os.environ.get('PGUSER'),
+            password=os.environ.get('PGPASSWORD')
+        ),
+        pool_size=1,
+        max_overflow=1
+    )
+    yield engine
+    engine.dispose()
+
+
+def execute_query(engine):
+    """Execute query."""
+    query_results = []
+    logger.info('engine.connect()')
+    with engine.connect() as connection:
+        logger.info('connection.execute()')
+        rows = connection.execute(
+            text("SELECT * FROM FruitsMenu")
+        )
+        for row in rows:
+            query_results.append({'name': row['name'], 'price': row['price']})
+            # logger.info(f"name={row['name']} price={row['price']}")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(query_results)
+    }
+
+
+def main(driver_name):
+    """Run main."""
+    result = {
+        'statusCode': 500,
+        'body': 'Internal Server Error.'
+    }
+    try:
+        with create_engine(driver_name) as engine:
+            t_0 = time.time()
+            logger.info(execute_query(engine))
+            t_1 = time.time()
+            logger.info(f'dt = {(t_1 - t_0):.3f}s')
+
+    except sqlalchemy.exc.ProgrammingError as exc:
+        logger.error(traceback.format_exc())
+        logger.error(exc)
+        result = {
+            'statusCode': 500,
+            'body': json.dumps(traceback.format_exc())
+        }
+
+    return result
+
+
+if __name__ == '__main__':
+    if sys.argv[1] in ['postgresql+pg8000', 'postgresql+psycopg2']:
+        main(sys.argv[1])
+    else:
+        print("usage: {sys.argv[0]} " + '{postgresql+pg8000|postgresql+psycopg2}', sys.stderr)
+
+# EOF
