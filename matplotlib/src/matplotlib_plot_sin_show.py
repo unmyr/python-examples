6018ec34f3fc258106e40c6eec0f57215a630abe
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-18 16:39:40 +0900

    feat: Improve example of a sine wave.

--- matplotlib/src/matplotlib_plot_sin_show.py
+++ matplotlib/src/matplotlib_plot_sin_show.py
@@ -1,9 +1,34 @@
 # -*- coding: utf-8 -*-
 """Plot of Sine"""
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-10.0, 10.0, 0.1)
-y = np.sin(x)
-plt.plot(x, y)
-plt.show()
+
+def main():
+    """Run main."""
+    x = np.arange(-math.pi, math.pi, 0.2)
+    y1 = np.sin(x)
+    y2 = np.cos(x)
+
+    fig = plt.figure(figsize=(6, 4))
+
+    ax = fig.add_subplot(1, 1, 1)  # Create one plot area.
+    ax.set_title('Sine wave')
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$y$')
+
+    ax.plot(x, y1, label=r'$\sin(x)$')
+    ax.plot(x, y2, label=r'$\cos(x)$')
+    ax.legend()
+
+    fig.savefig('matplotlib_plot_sin.png')
+
+    fig.tight_layout()
+    fig.show()
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
