cee1d1d5dcb51002751f8b495f9c444bbfa5d10f
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-18 19:07:22 +0900

    refactor: Refactor codes.

--- matplotlib/src/feasible_region.py
+++ matplotlib/src/feasible_region.py
@@ -3,36 +3,72 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# plot the feasible region
-d = np.linspace(-2, 16, 300)
-x, y = np.meshgrid(d, d)
-plt.imshow(
-    ((y >= 2) & (2 * y <= 25 - x) & (4 * y >= 2 * x - 8) & (y <= 2 * x - 5)).astype(int),
-    extent=(x.min(), x.max(), y.min(), y.max()),
-    origin="lower",
-    cmap="Greys",
-    alpha=0.3)
 
-# plot the lines defining the constraints
-x = np.linspace(0, 16, 2000)
-# y >= 2
-y1 = (x * 0) + 2
-# 2y <= 25 - x
-y2 = (25 - x) / 2.0
-# 4y >= 2x - 8
-y3 = (2 * x - 8) / 4.0
-# y <= 2x - 5
-y4 = 2 * x - 5
+def main():
+    """Run main."""
+    # plot the feasible region
+    x_min = -10
+    x_max = 100
+    y_min = -10
+    y_max = 100
+    mesh_x1, mesh_x2 = np.meshgrid(
+        np.linspace(x_min, x_max, 300),
+        np.linspace(y_min, y_max, 300)
+    )
 
-# Make plot
-plt.plot(x, 2 * np.ones_like(y1))
-plt.plot(x, y2, label=r'$2y\leq25-x$')
-plt.plot(x, y3, label=r'$4y\geq 2x - 8$')
-plt.plot(x, y4, label=r'$y\leq 2x-5$')
-plt.xlim(0, 16)
-plt.ylim(0, 11)
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-plt.xlabel(r'$x$')
-plt.ylabel(r'$y$')
-plt.savefig('feasible_region.png')
-# plt.show()
+    fig = plt.figure(figsize=(12, 8))
+
+    ax = fig.add_subplot(1, 1, 1)  # Create one plot area.
+    ax.set_title('LP')
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$y$')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    ax.imshow(
+        (
+            (mesh_x1 >= 0)
+            & (mesh_x2 >= 0)
+            & (mesh_x1 + 2 * mesh_x2 <= 80)
+            & (4 * mesh_x1 + 4 * mesh_x2 <= 180)
+            & (3 * mesh_x1 + mesh_x2 <= 90)
+        ).astype(int) * (5 * mesh_x1 + 4 * mesh_x2),
+        extent=(mesh_x1.min(), mesh_x1.max(), mesh_x2.min(), mesh_x2.max()),
+        origin="lower",
+        cmap="Blues",
+        alpha=0.8
+    )
+
+    # plot the lines defining the constraints
+    x1 = np.linspace(x_min, x_max, 2000)
+    # y >= 0
+    y1 = x1 * 0
+    # x_1 + 2 x_2 <= 80
+    y2 = (80 - x1) / 2.0
+    # 4 x_1 + 4 x_2 <= 180
+    y3 = (180 - 4 * x1) / 4.0
+    # 3 x_1 + x_2 <= 90
+    y4 = (90 - 3 * x1)
+
+    # 0 <= x
+    x0 = np.zeros(2000)
+    y0 = np.linspace(y_min, y_max, 2000)
+
+    # Make plot
+    plt.plot(x0, y0, label=r'$x=0$')
+    plt.plot(x1, y1, label=r'$y=0$')
+    plt.plot(x1, y2, label=r'$x_1 + 2 x_2 \leq 80$')
+    plt.plot(x1, y3, label=r'$4 x_1 + 4 x_2 \leq 180$')
+    plt.plot(x1, y4, label=r'$3 x_1 + x_2 \leq 90$')
+
+    plt.legend()
+
+    fig.tight_layout()
+    fig.show()
+    fig.savefig('feasible_region.png')
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
