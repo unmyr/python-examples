3ce194acaa7f47bd3016ffbf5dc1ccef40e6a439
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-22 01:17:47 +0900

    refactor: Fix linter warnings.

--- src/time/time_delta.py
+++ src/time/time_delta.py
@@ -10,14 +10,14 @@ def main():
     t_0 = time.time()
     time.sleep(0.8)
     t_1 = time.time()
-    print(type(t_1-t_0))
-    print("dt = %f" % (t_1-t_0))
+    print(type(t_1 - t_0))
+    print("dt = %f" % (t_1 - t_0))
 
     t_0 = datetime.now()
     time.sleep(0.8)
     t_1 = datetime.now()
     print(type(t_1))
-    d_t = (t_0-t_1).seconds
+    d_t = (t_0 - t_1).seconds
     print("dt = %d" % d_t)
 
 
