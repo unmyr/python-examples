f3e06a4b08b61e65f94127987e5be5c35b4131c9
unmyr <56982541+unmyr@users.noreply.github.com>
2021-05-17 22:23:23 +0900

    feat: Add a example of select with sqlite3.

---
+++ database/sqlite3/src/sqlite3_select.py
@@ -0,0 +1,26 @@
+"""Example of execute with sqlite3."""
+import sqlite3
+
+con = sqlite3.connect(":memory:")
+cur = con.cursor()
+cur.execute("CREATE TABLE lang (lang_name, lang_age)")
+
+# This is the qmark style:
+cur.execute("INSERT INTO lang VALUES (?, ?)", ("C", 49))
+
+# The qmark style used with executemany():
+lang_list = [
+    ("Fortran", 64),
+    ("Python", 30),
+    ("Go", 11),
+]
+cur.executemany("INSERT INTO lang VALUES (?, ?)", lang_list)
+
+# And this is the named style:
+cur.execute("SELECT * FROM lang WHERE lang_name=:name AND lang_age=:age", {
+    "name": "C",
+    "age": 49
+})
+print(cur.fetchall())
+
+con.close()
