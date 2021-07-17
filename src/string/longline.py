57eb67ad8ca5f40f84aee0735cd073e50eb564e6
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-17 15:03:07 +0900

    feat(string): Add a example of long string.

---
+++ src/string/longline.py
@@ -0,0 +1,10 @@
+"""Example of long string"""
+key_name = 'Apple'
+long_str = (
+    "SELECT *"
+    " FROM fruits_menu"
+    f" WHERE name='{key_name}'"
+)
+
+print(long_str)
+assert long_str == "SELECT * FROM fruits_menu WHERE name='Apple'"
