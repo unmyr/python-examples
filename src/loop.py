a6d4ff5bb1e6281c88c87e8ec882c6e78629edb0
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-17 15:07:29 +0900

    feat: Add a example of enumerate.

--- src/loop.py
+++ src/loop.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Python for Loop explained with examples."""
+makers = ['Toyota', 'Nissan', 'Honda']
+for maker_name in makers:
+    print(maker_name)
 
-for i in ['Toyota', 'Nissan', 'Honda']:
-    print(i)
+for i, maker_name in enumerate(makers):
+    print(f'{i}: {maker_name}')
 
 for i in range(5):
     print(i)
