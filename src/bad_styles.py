8b34808ad0a3c3457afb78b45b0ba905c6306767
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-17 15:11:26 +0900

    feat: Add example of pylint.

---
+++ src/bad_styles.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Bad style example."""
+
+
+def main():
+    """main."""
+    # pylint: disable=redefined-builtin
+    str: int = 5 * 4
+    print(str)
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
