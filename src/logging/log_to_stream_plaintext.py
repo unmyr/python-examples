ac69e06da60565b280b0ffd97b60f88892327648
unmyr <56982541+unmyr@users.noreply.github.com>
2021-08-04 21:04:36 +0900

    refactor(logging): Add exception handling.

--- src/logging/log_to_stream_plaintext.py
+++ src/logging/log_to_stream_plaintext.py
@@ -1,10 +1,29 @@
 """Example of logger."""
 from logging import getLogger, StreamHandler, DEBUG
-logger = getLogger(__name__)
-handler = StreamHandler()
-handler.setLevel(DEBUG)
-logger.setLevel(DEBUG)
-logger.addHandler(handler)
-logger.propagate = False
+import sys
+import traceback
 
-logger.debug("hello world!")
+try:
+    logger = getLogger(__name__)
+    handler = StreamHandler()
+    handler.setLevel(DEBUG)
+    logger.setLevel(DEBUG)
+    logger.addHandler(handler)
+    logger.propagate = False
+
+    logger.debug("hello world!")
+
+except TypeError as exc:
+    print(f"{exc.args}", file=sys.stderr)
+    for attr_name in dir(exc):
+        try:
+            if hasattr(exc, attr_name):
+                attr_obj = getattr(exc, attr_name)
+                print(f"attr={attr_name}, type={type(attr_obj)}, callable={callable(attr_obj)}")
+            else:
+                print(f"attr={attr_name}")
+
+        except AttributeError as exc2:
+            print(type(exc2))
+            print(traceback.format_exc())
+            print(f"attr={attr_name}")
---
+++ tests/test_logging.py
@@ -0,0 +1,41 @@
+"""Test logging with unittest."""
+import unittest
+import logging
+
+
+class TestCalc(unittest.TestCase):
+    """Test logging module."""
+
+    def test_handler_set_level_none(self):
+        """Test dict."""
+        with self.assertRaises(TypeError) as context:
+            logger = logging.getLogger(__name__)
+            handler = logging.StreamHandler()
+            handler.setLevel(None)  # NG
+            logger.setLevel(logging.DEBUG)
+            logger.addHandler(handler)
+            logger.propagate = False
+
+            logger.debug("hello world!")
+
+        self.assertEqual(
+            "Level not an integer or a valid string: None",
+            str(context.exception)
+        )
+
+    def test_logger_set_level_none(self):
+        """Test dict."""
+        with self.assertRaises(TypeError) as context:
+            logger = logging.getLogger(__name__)
+            handler = logging.StreamHandler()
+            handler.setLevel(logging.DEBUG)
+            logger.setLevel(None)  # NG
+            logger.addHandler(handler)
+            logger.propagate = False
+
+            logger.debug("hello world!")
+
+        self.assertEqual(
+            "Level not an integer or a valid string: None",
+            str(context.exception)
+        )
