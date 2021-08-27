1a89312d5913569096404ef64d8a67b0320aae0f
unmyr <56982541+unmyr@users.noreply.github.com>
2021-08-27 22:14:45 +0900

    test(list): Add a example of list.

---
+++ tests/test_list.py
@@ -0,0 +1,28 @@
+"""Test list with unittest."""
+import unittest
+
+
+class TestList(unittest.TestCase):
+    """Test list module."""
+
+    def test_sort(self):
+        """Test sorted."""
+        list_of_dict = [
+            {"name": "a", "value": 0.1},
+            {"name": "b", "value": 1.1},
+            {"name": "c", "value": 0.5}
+        ]
+
+        sorted_new_list = sorted(
+            list_of_dict,
+            key=lambda k: k['value'], reverse=True
+        )
+
+        self.assertListEqual(
+            sorted_new_list,
+            [
+                {"name": "b", "value": 1.1},
+                {"name": "c", "value": 0.5},
+                {"name": "a", "value": 0.1}
+            ]
+        )
