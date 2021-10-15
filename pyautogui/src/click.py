df2e1b29efcdb100ac06aa07a2dc76cbe9da2fc3
unmyr <56982541+unmyr@users.noreply.github.com>
2021-10-15 23:52:48 +0900

    feat(pyautogui): Add examples.

---
+++ pyautogui/src/click.py
@@ -0,0 +1,23 @@
+"""Example of a pyautogui."""
+import math
+
+import pyautogui
+
+
+def main():
+    """Run main."""
+    error = 0
+    tx, ty = 159, 435
+    pyautogui.moveTo(tx, ty)
+    for _ in range(10000):
+        p = pyautogui.position()
+        error += math.sqrt((p[0] - tx)**2 + (p[1] - ty)**2)
+        if error > 1000:
+            break
+        pyautogui.click(tx, ty)
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
---
+++ pyautogui/src/screenshot.py
@@ -0,0 +1,13 @@
+"""Example of a pyautogui."""
+import pyautogui
+
+
+def main():
+    """Run main."""
+    pyautogui.screenshot('hoge.png', region=(10, 117, 1050, 894))
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
