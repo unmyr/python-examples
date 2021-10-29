20a83d44d8a04c2a78625c35c4a374c57a7096cf
unmyr <56982541+unmyr@users.noreply.github.com>
2021-10-29 23:25:28 +0900

    feat(tracemalloc): Add a minimal example.

---
+++ src/tracemalloc/memleak_minimal.py
@@ -0,0 +1,27 @@
+"""Example of tracemalloc."""
+import tracemalloc
+
+
+def main():
+    """Run main."""
+    tracemalloc.start()
+
+    snapshot1 = tracemalloc.take_snapshot()
+
+    my_array = [None] * 1024  # pylint: disable=unused-variable
+    # del my_array
+
+    snapshot2 = tracemalloc.take_snapshot()
+    top_stats = snapshot2.compare_to(snapshot1, 'lineno')
+
+    print("[ Top 10 ]")
+    for stat in top_stats[:10]:
+        print(stat)
+
+    tracemalloc.stop()
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
