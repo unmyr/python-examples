307dab4cbede0748b4a8ec7910ea63d804161313
unmyr <56982541+unmyr@users.noreply.github.com>
2021-11-03 15:27:31 +0900

    feat(typing): Improve examples.

--- src/typing/func.py
+++ src/typing/func.py
@@ -24,17 +24,10 @@ def get_now() -> 'datetime.datetime':
     return datetime.datetime.now()
 
 
-def append_int(
-    in_list: typing.List[int], val: int
-) -> typing.List[int]:
-    """Get string."""
-    in_list.append(val)
-    return in_list
-
-
 def get_multiple_returns(
     ret_kind: int
-) -> typing.Tuple[str, typing.Union[typing.List, typing.Dict, bytes, str, int, float]]:
+) -> typing.Tuple[str, typing.Union[typing.List, typing.Dict, bytes, str, int,
+                                    float]]:
     """Example of multiple returns."""
     if ret_kind == 0:
         return "Hello", 3.14
@@ -55,21 +48,6 @@ class User:
     def get_age(self) -> int:
         """Get age."""
         return self.age
-
-
-def get_timestamps(
-) -> typing.Tuple[User, typing.List[typing.Union[float, typing.List[int]]]]:
-    """Get current datetime."""
-    perf_list: typing.List[typing.Union[float, typing.List[int]]]
-    perf_list = []
-    t_0 = datetime.datetime.now()
-    t_1 = datetime.datetime.now()
-    t_2 = datetime.datetime.now()
-    perf_list.append(round((t_1 - t_0).total_seconds(), 3))
-    perf_list.append(round((t_2 - t_1).total_seconds(), 3))
-    perf_list.insert(0, round((t_2 - t_0).total_seconds(), 3))
-    perf_list.append([1, 2])
-    return User('Bob', 28), perf_list
 
 
 def main() -> None:
@@ -98,14 +76,6 @@ def main() -> None:
         get_multiple_returns(1)
     )
     print(f"{str_val}, {bool_val} = get_multiple_returns(1)")
-
-    number_List: typing.Iterable = append_int([1, 2, 3], 4)
-    for elem in number_List:
-        print(elem)
-
-    result: typing.List
-    user, result = get_timestamps()
-    print(f"{user} {result}")
 
 
 if __name__ == '__main__':
---
+++ src/typing/list_typing.py
@@ -0,0 +1,39 @@
+"""Example of typing."""
+import typing
+
+
+def get_my_list() -> typing.List[typing.Union[float, typing.List[int],
+                                              typing.Tuple[float, int, str]]]:
+    """Get current datetime."""
+    my_list: typing.List[typing.Union[float, typing.List[int],
+                                      typing.Tuple[float, int, str]]]
+    my_list = []
+    my_list.append(3.14)
+    my_list.append([1, 2])
+    my_list.append((3.14, 4, 'Hello'))
+    return my_list
+
+
+def append_int(
+    in_list: typing.List[int], val: int
+) -> typing.List[int]:
+    """Get string."""
+    in_list.append(val)
+    return in_list
+
+
+def main() -> None:
+    """Run main."""
+    my_list: typing.Iterable
+    my_list = get_my_list()
+    for item in my_list:
+        print(item)
+
+    number_List: typing.Iterable = append_int([1, 2, 3], 4)
+    print(list(map(lambda x: x * x, number_List)))
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
