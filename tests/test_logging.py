fbb19a4f18e35994044541f5b02735b7a6a58bd7
unmyr <56982541+unmyr@users.noreply.github.com>
2021-11-04 22:31:52 +0900

    test(logging): Add an example of `getLevelName`.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -5,7 +5,6 @@ import logging
 
 class TestLogging(unittest.TestCase):
     """Test logging module."""
-
     def test_handler_set_level_none(self):
         """Test the handler instance."""
         with self.assertRaises(TypeError) as context:
@@ -18,10 +17,8 @@ class TestLogging(unittest.TestCase):
 
             logger.debug("hello world!")
 
-        self.assertEqual(
-            "Level not an integer or a valid string: None",
-            str(context.exception)
-        )
+        self.assertEqual("Level not an integer or a valid string: None",
+                         str(context.exception))
 
     def test_logger_set_level_none(self):
         """Test the logger instance."""
@@ -35,7 +32,23 @@ class TestLogging(unittest.TestCase):
 
             logger.debug("hello world!")
 
-        self.assertEqual(
-            "Level not an integer or a valid string: None",
-            str(context.exception)
-        )
+        self.assertEqual("Level not an integer or a valid string: None",
+                         str(context.exception))
+
+    def test_logging_level_name(self):
+        """Test the logging level representation."""
+        self.assertEqual(logging.getLevelName('NOTSET'), logging.NOTSET)
+        self.assertEqual(logging.getLevelName('DEBUG'), logging.DEBUG)
+        self.assertEqual(logging.getLevelName('INFO'), logging.INFO)
+        self.assertEqual(logging.getLevelName('WARNING'), logging.WARNING)
+        self.assertEqual(logging.getLevelName('ERROR'), logging.ERROR)
+        self.assertEqual(logging.getLevelName('CRITICAL'), logging.CRITICAL)
+        self.assertEqual(logging.getLevelName('FATAL'), logging.CRITICAL)
+        self.assertEqual(logging.getLevelName('Info'), 'Level Info')
+
+        self.assertEqual(logging.getLevelName(logging.NOTSET), 'NOTSET')
+        self.assertEqual(logging.getLevelName(logging.DEBUG), 'DEBUG')
+        self.assertEqual(logging.getLevelName(logging.INFO), 'INFO')
+        self.assertEqual(logging.getLevelName(logging.WARNING), 'WARNING')
+        self.assertEqual(logging.getLevelName(logging.ERROR), 'ERROR')
+        self.assertEqual(logging.getLevelName(logging.CRITICAL), 'CRITICAL')
