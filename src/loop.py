b87e130dab0a0f9efcd7e4d33b16ae71e6e78ae8
unmyr <56982541+unmyr@users.noreply.github.com>
2021-11-09 21:23:56 +0900

    feat(loop): Add an example of `continue`.

--- src/loop.py
+++ src/loop.py
@@ -1,12 +1,29 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 """Python for Loop explained with examples."""
-makers = ['Toyota', 'Nissan', 'Honda']
-for maker_name in makers:
-    print(maker_name)
 
-for i, maker_name in enumerate(makers):
-    print(f'{i}: {maker_name}')
 
-for i in range(5):
-    print(i)
+def main() -> None:
+    """Run main."""
+    makers = ['Toyota', 'Nissan', 'Honda']
+    for maker_name in makers:
+        print(maker_name)
+
+    for i, maker_name in enumerate(makers):
+        print(f'{i}: {maker_name}')
+
+    ary = []
+    for i in range(5):
+        ary.append(i)
+    assert ary == [0, 1, 2, 3, 4]
+
+    ary.clear()
+    for i in range(5):
+        if i == 3:
+            continue
+        ary.append(i)
+    assert ary == [0, 1, 2, 4]
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
