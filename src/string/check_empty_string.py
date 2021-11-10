114d97a248a2dc965586af7ede0e39e8b2fb46c6
unmyr <56982541+unmyr@users.noreply.github.com>
2021-11-10 23:43:08 +0900

    feat(string): Add an example to detect an empty string.

---
+++ src/string/check_empty_string.py
@@ -0,0 +1,12 @@
+"""Check string is empty."""
+str_value = ''
+if str_value:
+    print("The string is not empty.")
+else:
+    print('The string is empty.')
+
+str_value = 'Hello'
+if str_value:
+    print(f"The string is not empty. : VALUE='{str_value}'")
+else:
+    print('The string is empty.')
