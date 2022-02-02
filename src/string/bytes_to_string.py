7e88ae7dbf7af34152ba41f19d9de722463e21f0
unmyr <56982541+unmyr@users.noreply.github.com>
2022-02-02 21:13:54 +0900

    feat(string): Add an example of converting bytes to a string.

---
+++ src/string/bytes_to_string.py
@@ -0,0 +1,12 @@
+"""Bytes to string."""
+
+
+def main() -> None:
+    """Run main."""
+    assert b"Hello world.".decode('utf-8') == "Hello world."
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
