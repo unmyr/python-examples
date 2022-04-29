0722e122a772da8ef206d53ffe9af1aabc3eac06
unmyr <56982541+unmyr@users.noreply.github.com>
2022-04-29 19:18:16 +0900

    refactor(packages): Specify the license in the metadata.

--- packages/bdist_example_py3_dynamic/setup.py
+++ packages/bdist_example_py3_dynamic/setup.py
@@ -12,7 +12,15 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://example.com/',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
 )
