920e2170fda83ff907a681ca4cc0080be8da9e32
unmyr <56982541+unmyr@users.noreply.github.com>
2025-05-18 03:26:59 +0900

    feat(sympy): Refactor verification logic and simplify function application in seminar 2025 verification script for 数学セミナー

--- sympy/src/seminar_2025_04_verify_with_num.py
+++ sympy/src/seminar_2025_04_verify_with_num.py
@@ -5,17 +5,17 @@ from sympy import sqrt
 def main() -> None:
     """Run main."""
     # Definition: Variables and functions
-    x = sympy.symbols("x")
-    f_1 = 2 * x / (1 - x**2)
-    # f_2 = sympy.simplify(2 * f_1 / (1 - f_1**2))
-    # f_3 = sympy.simplify(2 * f_2 / (1 - f_2**2))
-    # f_4 = sympy.simplify(2 * f_3 / (1 - f_3**2))
+    x, y, z, w = sympy.symbols("x y z w")
+    y_x = 2 * x / (1 - x**2)
+    z_y = sympy.simplify(2 * y / (1 - y**2))
+    w_z = sympy.simplify(2 * z / (1 - z**2))
+    x_w = sympy.simplify(2 * w / (1 - w**2))
 
     # Define the list of initial values
     initial_values = [
         sympy.Number(0),
-        sympy.conjugate(-sympy.I),
-        sympy.conjugate(sympy.I),
+        -sympy.I,
+        sympy.I,
         -sqrt(3),
         sqrt(3),
         -sqrt(5 - 2 * sqrt(5)),
@@ -38,23 +38,21 @@ def main() -> None:
     # Apply the function f to the initial values and output the results
     for x_in in initial_values:
         # Apply the function f four times
-        y = complex(f_1.subs(x, x_in))
-        z = complex(f_1.subs(x, y))
-        w = complex(f_1.subs(x, z))
-        x_out = complex(f_1.subs(x, w))
-        # y = sympy.simplify(f_1.subs(x, x_in))
-        # z = sympy.simplify(f_2.subs(x, x_in))
-        # w = sympy.simplify(f_3.subs(x, x_in))
-        # x_out = sympy.simplify(f_4.subs(x, x_in))
+        y_out = y_x.subs(x, complex(x_in))
+        z_out = z_y.subs(y, complex(y_out))
+        w_out = w_z.subs(z, complex(z_out))
+        x_out = x_w.subs(w, complex(w_out))
+
+        # Calculate the error
         error = abs(x_out - x_in)
 
         print(
             f"x={complex(x_in):+2.3f}: "
             + ",".join(
                 [
-                    f"f^1={complex(y):+.3f}",
-                    f"f^2={complex(z):+.3f}",
-                    f"f^3={complex(w):+.3f}",
+                    f"f^1={complex(y_out):+.3f}",
+                    f"f^2={complex(z_out):+.3f}",
+                    f"f^3={complex(w_out):+.3f}",
                     f"f^4={complex(x_out):+.3f}",
                     f"error={error:.3f}",
                 ]
